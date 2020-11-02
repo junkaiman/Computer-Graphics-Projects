a0b39ab9601ed5306305606a0b5358e6df85b075
keon <junkai.man@dukekunshan.edu.cn>
2020-11-02 09:23:38 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# MEDIART206
+# Homework for ComputerGraphics2020
+
+Student NetID: jm687
+
+Student Name: Junkai Man
+
+
+
+
+
+MEDIART 206: Computer Graphics - openFrameworks
+
+Prof. Benjamin Bacon
+
+Fall Session 2 2020
+
---
+++ Week 1/week01class02_bouncingballs/src/ofApp.cpp
@@ -0,0 +1,182 @@
+#include "ofApp.h"
+#include "math.h"
+int bgColor = 0;
+int nextTriggerChangingBallColor = 0;
+int nextTriggerChangingBgColor = 0;
+int bgChangingDir = 1;
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+	ofBackground(bgColor);
+
+	int numBalls = 10;
+
+	for (int i = 0; i < numBalls; i++) {
+		Ball tempBall;
+		balls.push_back(tempBall);
+	}
+	// there's actually a bug, if two balls randomly spawn as two overlapped circles,
+	// they will shake with each other
+
+	// i tried to fix this bug using code below that was commented out,
+	// but i might made some mistakes
+
+	//Ball tempBall;
+	//balls.push_back(tempBall);
+
+	//for (int i = 0; i < numBalls; i++) {		// loop 30 times
+
+	//	Ball tempBall;					// create a temporary ball
+	//	for (int j = 0; j < balls.size(); j++) {
+	//		if (pow(tempBall.x - balls[j].x, 2) + pow(tempBall.y - balls[j].y, 2)
+	//			> pow(tempBall.radius + balls[j].radius, 2)) {
+
+	//			// only when two balls now overlap, push into the vector
+	//			balls.push_back(tempBall);		// copy the temporary ball into the balls vector
+	//											// (push_back() adds the ball to the end of the vector, increasing the vector's size by 1)
+	//		}
+	//	}
+		
+	//}
+	
+}
+
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	// update with ball collision
+	for (int i = 0; i < balls.size() - 1; i++) {
+		for (int j = i + 1; j < balls.size(); j++) {
+			if (pow(balls[i].x - balls[j].x, 2) + pow(balls[i].y - balls[j].y, 2)
+				< pow(balls[i].radius + balls[j].radius, 2) + 1) {
+				// haven't figure it out how to deal with the collision,
+				// i've tried to use ==, but it doesn't work successfully,
+				// so I add a "< 1" to it
+				// and sometimes two balls will collide forever untill a third ball collides with them
+				balls[i].collide();
+				balls[j].collide();
+			}
+		}
+	}
+
+	// update each ball in our vector
+	for (int i = 0; i < balls.size(); i++) {	// one way to loop through a whole vector
+		balls[i].update();	// we can access an individual ball by its "index" number in the vector
+							// e.g. balls[0] is the first ball in the vector, balls[1] is the second ball, etc.
+									// (note that vectors always start at 0, not 1!!)
+	}
+
+
+
+	// i learned this way of changing color every certain time interval from here:
+	// https://forum.openframeworks.cc/t/triggering-a-function-or-event-only-once-at-a-certain-hour-c-question/6169
+	int nowTime = ofGetElapsedTimeMillis();
+	if (nowTime > nextTriggerChangingBallColor) {
+		// change color of the ball
+		for (int i = 0; i < balls.size(); i++) {
+			balls[i].color = ofColor(ofRandom(0, 255), ofRandom(0, 255), ofRandom(0, 255));
+		}
+		// every 10 milli seconds
+		nextTriggerChangingBallColor = nowTime + 10;
+
+		
+	}
+
+	if (nowTime > nextTriggerChangingBgColor) {
+		// change color of the background, smoothly
+
+		// if turning white
+		if (bgChangingDir > 0) {
+			if (bgColor < 255) {
+				ofBackground(bgColor);
+				bgColor += 1;
+			}
+			else {
+				// to the black
+				bgChangingDir = - bgChangingDir;
+			}
+		}
+		// if turning black
+		else {
+			if (bgColor > 0) {
+				ofBackground(bgColor);
+				bgColor -= 1;
+			}
+			else {
+				// to the white
+				bgChangingDir = - bgChangingDir;
+			}
+		}
+		// every 10 milliseconds
+		nextTriggerChangingBgColor = nowTime + 10;
+
+	}
+
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+	for (int i = 0; i < balls.size(); i++) {
+
+		balls[i].draw();
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
